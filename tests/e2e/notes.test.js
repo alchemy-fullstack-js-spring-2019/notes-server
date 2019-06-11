080c98ac791b752f8e509f6b35036b193331e495
Ryan Mehta <ryan.mehta@gmail.com>
2019-06-10 17:05:46 -0700

    notes list

--- tests/e2e/notes.test.js
+++ tests/e2e/notes.test.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose');
 const connect = require('../../lib/utils/connect');
 const app = require('../../lib/app');
 
+const createNote = note => {
+  return request(app)
+    .post('/api/v1/notes')
+    .send(note)
+    .then(res => res.body);
+};
+
 describe('notes routes', () => {
   beforeAll(() => {
     return connect();
@@ -28,6 +35,20 @@ describe('notes routes', () => {
           body: 'my note has a body',
           __v: 0
         });
+      });
+  });
+
+  it('can get a list of notes', async() => {
+    const notes = await Promise.all([
+      createNote({ title: 'hi', body: 'there' }),
+      createNote({ title: 'hi', body: 'there1' }),
+      createNote({ title: 'hi', body: 'there2' })
+    ]);
+
+    return request(app)
+      .get('/api/v1/notes')
+      .then(res => {
+        expect(res.body).toEqual(notes);
       });
   });
 });
